4f790274c5f45fc20d88defe753c822e4a8d43ad
KseniaShkolnikova <isip_k.v.shkolnikova@mpt.ru>
2023-11-07 17:20:33 +0300

    convert

---
+++ текстовый конверт/Class1.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace текстовый_конверт
+{
+    public class ocnov
+    {
+        public string figura;
+        public string pervoe;
+        public string vtoroe;
+        public string tretie;
+
+    private static string[] massiv (string []per, List<ocnov> text)
+        {
+            foreach (var item in text)
+            {
+                per[0] = item.figura;
+                per[1] = item.pervoe;
+                per[2] = item.vtoroe;
+                per[3] = item.tretie;
+            }
+            return per;
+        }
+        private static List<ocnov> vivod(List<ocnov> text)
+        {
+            foreach (var item in text)
+            {
+                Console.WriteLine(item.figura);
+                Console.WriteLine(item.pervoe);
+                Console.WriteLine(item.vtoroe);
+                Console.WriteLine(item.tretie);
+            }
+            return text;
+        }
+        public static List<ocnov> txtvivod(string [] per, string asd, List<ocnov>text, ocnov figuri)
+        {
+            per = File.ReadAllLines(asd);
+            foreach (var line in text)
+            {
+                Console.WriteLine(line);
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                figuri.figura = per[0];
+                figuri.pervoe = per[1];
+                figuri.vtoroe = per[2];
+                figuri.tretie = per[3];
+            }
+            text.Add(figuri);
+            vivod(text);
+            return text;
+        }
+        public static List<ocnov> xmlvivod( string asd, List<ocnov> text, string[] per)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(List<ocnov>));
+            using (FileStream xm = new FileStream(asd, FileMode.Open))
+            {
+                text = (List<ocnov>)xml.Deserialize(xm);
+            }
+            massiv(per, text);
+            vivod(text);
+            return text;
+        }
+        public static List<ocnov> jsonvivod(string asd, List<ocnov> text, string[]per)
+        {
+            string ret = File.ReadAllText(asd);
+            text = JsonConvert.DeserializeObject<List<ocnov>>(ret);
+            massiv(per, text);
+            vivod(text);
+            return text;
+        }
+        public static List<ocnov> txtvvod(string[] per, List<ocnov> text, string asd)
+        {
+            massiv(per,text);
+            File.WriteAllLines(asd, per);
+            return text;
+        }
+        public static List<ocnov> xmlvvod(string asd, List<ocnov> text, string[]per)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(List<ocnov>));
+            using (FileStream xm = new FileStream(asd, FileMode.OpenOrCreate))
+            {
+                xml.Serialize(xm, text);
+            }
+            massiv(per, text);
+            return text;
+        }
+        public static List<ocnov> jsonvvod(string asd, List<ocnov> text,ocnov figuri, string[]per)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(List<ocnov>));
+            using (FileStream xm = new FileStream(asd, FileMode.OpenOrCreate))
+            {
+                xml.Serialize(xm, text);
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                figuri.figura = per[0];
+                figuri.pervoe = per[1];
+                figuri.vtoroe = per[2];
+                figuri.tretie = per[3];
+            }
+            return text;
+        }
+
+       
+    }
+    
+}
---
+++ текстовый конверт/Program.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Runtime.ConstrainedExecution;
+using System.Xml.Serialization;
+
+namespace текстовый_конверт
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ocnov figuri = new ocnov();
+            List<ocnov> text = new List<ocnov>();
+            string[] per = new string[4];
+            ConsoleKeyInfo der;
+            do
+            {
+                text.Clear();
+                Console.WriteLine("Введите путь до файла, который вы хотите открыть");
+                Console.WriteLine("-------------------------------------------------");
+                string asd = Console.ReadLine();
+                if (asd.EndsWith(".txt"))
+                {            
+                    ocnov.txtvivod (per, asd, text, figuri);
+                }
+                else if (asd.EndsWith(".xml"))
+                {
+                    ocnov.xmlvivod(asd, text, per);
+
+                }
+                else if (asd.EndsWith(".json"))
+                {
+                    ocnov.jsonvivod(asd, text, per);
+
+                }
+                Console.WriteLine("Сохраните файл в один из 3 форматов (txt,xml,json)-F1 и путь до файла, для выхода - Escape");
+                Console.WriteLine("--------------------------------------------------------------------------------------------");
+                der = Console.ReadKey();
+                if (der.Key== ConsoleKey.F1)
+                {
+                    Console.WriteLine("");
+                    asd = Console.ReadLine();
+                    if (asd.EndsWith(".txt"))
+                    {
+                        ocnov.txtvvod(per, text, asd);
+                    }
+                    else if (asd.EndsWith(".xml"))
+                    {
+                        ocnov.xmlvvod(asd, text, per);
+                    }
+                    else if (asd.EndsWith(".json"))
+                    {
+                        ocnov.jsonvvod(asd, text, figuri, per);
+                    }
+                    Console.Clear();
+                }
+                else if (der.Key != ConsoleKey.Escape && der.Key != ConsoleKey.F1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Попробуйте еще раз");
+                }
+                
+
+            } while (der.Key != ConsoleKey.Escape);
+        }
+    }
+}
